d3cb6559a8bfb018b2247ebcf715b04751d34564
Steve Alden <stevealden.77@gmail.com>
2020-02-03 18:16:12 +0530

    Updates

---
+++ precache-manifest.6c785d685ccc751d2aad95511899a1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4260e147b4e6c93ab7893851e6c2b2bf",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "abde9a0fefe690a22237",
+    "url": "/react-pokedex/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dcda092ecdcce8864ea4",
+    "url": "/react-pokedex/static/js/2.ed36ac18.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/react-pokedex/static/js/2.ed36ac18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "abde9a0fefe690a22237",
+    "url": "/react-pokedex/static/js/main.9da5cc26.chunk.js"
+  },
+  {
+    "revision": "a0fa66ac64f628a05b67",
+    "url": "/react-pokedex/static/js/runtime-main.303dafe9.js"
+  }
+]);
